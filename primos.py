19ab43584cf0638fbd55eaca3e5636417c3cc030
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-08-22 13:41:57 -0500

    Update primos.py

--- primos.py
+++ primos.py
@@ -1,32 +1,29 @@
-a = 1
-value = input('Ingrese un valor')
-value = int(value)
+a=1
+value=input("ingrese un numero")
+value=int(value)
 
-while a == 1:
-    for i in range(1,value+1):
-        conta = 0
-        for n in range(1, i+1):
-            residue = i%n
+while a==1:
+    for i in range(1,value + 1):
+        cont=0
+        for n in range(1,i +1):
+            residue=i%n
             if residue == 0:
-                conta = conta + 1
-            
-            # print("i = ", i)
-            # print("n = ", n)
-            # print("residue = ", residue)
-            # print("conta = ", conta)
-    if conta == 2:
-       print(f'{i} es un primo')
-       print("\n")
+                cont = cont + 1
+
+
+
+    if  cont==2 :
+        print(f" {i} es primo")
+        print("\n")
     else:
-       print(f'{i} NOOO es un primo')
+       print(f' {i} NO es un primo')
        print("\n")
 
-    print('Do you want to continue?. Press 1 to do that')
+    print('si quieres continuar presiona 1')
     a = input()
     a = int(a)
 
     if a != 1:
         break
-
     value = input('Ingrese un valor')
     value = int(value)
