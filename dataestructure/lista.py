211cbe068a797e63870ac969e31dd3aea6e08b2d
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-09-05 00:11:02 -0500

    Update lista.py

--- dataestructure/lista.py
+++ dataestructure/lista.py
@@ -1,1 +1,12 @@
+import time
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+#input()
+for i in range (0,len(my_lista)-1):
+    print(my_lista[i])
+    time.sleep(1)
+  
+    my_lista.append('Blanco')      
+    print(my_lista)
 
+    my_lista.insert(3, 'Negro')
+    print(my_lista)
