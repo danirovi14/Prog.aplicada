89fe93b9175db955b8cdbccad6885722cd748a61
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-10-03 13:14:45 -0500

    Create 5program.py

---
+++ Holamundo/5program.py
@@ -0,0 +1,13 @@
+coffee_price = 1.50
+number_of_coffees = 4
+ 
+
+print(coffee_price * number_of_coffees)
+print(coffee_price)
+print(number_of_coffees)
+ 
+coffee_price = 2.00
+ 
+print(coffee_price * number_of_coffees)
+print(coffee_price)
+print(number_of_coffees)
