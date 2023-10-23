752f4839608835d390f0dec71d48d1a0aa18725e
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-10-23 18:47:31 -0500

    Update methods.py

--- 2corte/methods.py
+++ 2corte/methods.py
@@ -9,4 +9,22 @@ teaching_table_area = circle.area(16/2)
 roun_room_area = circle.area(11460/2)
 
 
+#methods
+class Circle:
+  pi=3.14
+  def area(self,radius):
+    return Circle.pi*radius**2
+circle = Circle()
+pizza_area = circle.area(12/2)
+teaching_table_area = circle.area(16/2)
+roun_room_area = circle.area(11460/2)
+
+
+#constructors
+
+class Circle:
+  pi=3.14
+  def_init_(self,diameter):
+   print('New circle with diameter:{}'.format(diameter))
+teachig_table = Circle2(36)
 
