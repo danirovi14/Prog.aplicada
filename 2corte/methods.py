569349ccf338ce125e6dfae7b7dd266b08a9e522
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-10-23 18:57:40 -0500

    Actualizar methods.py

--- 2corte/methods.py
+++ 2corte/methods.py
@@ -27,4 +27,32 @@ class Circle:
   def_init_(self,diameter):
    print('New circle with diameter:{}'.format(diameter))
 teachig_table = Circle2(36)
+import math
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+
+    def calcular_area(self):
+        area = math.pi * (self.radio ** 2)
+        return area
+
+    def calcular_perimetro(self):
+        perimetro = 2 * math.pi * self.radio
+        return perimetro
+
+# Solicita al usuario que ingrese el radio del círculo
+radio = float(input("Ingresa el radio del círculo: "))
+
+# Crea una instancia de la clase Circulo
+mi_circulo = Circulo(radio)
+
+# Calcula el área y el perímetro del círculo
+area = mi_circulo.calcular_area()
+perimetro = mi_circulo.calcular_perimetro()
+
+# Imprime los resultados
+print("El área del círculo es:", area)
+print("El perímetro del círculo es:", perimetro)
+
 
