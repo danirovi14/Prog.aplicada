8bf63aa1997bddaf1c4bd0b96cf57ca7777a6137
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-10-23 19:09:46 -0500

    Update methods.py

--- 2corte/methods.py
+++ 2corte/methods.py
@@ -1,29 +1,21 @@
-#methods
-class Circcle:
-  pi=3.14
-  def area(self,radius):
-    return Circle.pi*radius**2
-circle = Circle()
-pizza_area = circle.area(12/2)
-teaching_table_area = circle.area(16/2)
-roun_room_area = circle.area(11460/2)
+class Circulo:
+    pi=3.1416
+    def __init__(self, radio):
+        self.radio = radio
+
+    def area(self):
+        area = Circulo.pi*self.radio ** 2
+        return area
+
+    def perimetro(self):
+        perimetro =Circulo.pi*2* self.radio
+        return perimetro
 
 
-#methods
-class Circle:
-  pi=3.14
-  def area(self,radius):
-    return Circle.pi*radius**2
-circle = Circle()
-pizza_area = circle.area(12/2)
-teaching_table_area = circle.area(16/2)
-roun_room_area = circle.area(11460/2)
+circulo = Circulo(5)
 
+area = circulo.area()
+perimetro = circulo.perimetro()
 
-#constructors
-
-class Circle:
-  pi=3.14
-  def_init_(self,diameter):
-   print('New circle with diameter:{}'.format(diameter))
-teachig_table = Circle2(36)
+print("El área del círculo es:", area)
+print("El perímetro del círculo es:", perimetro)
