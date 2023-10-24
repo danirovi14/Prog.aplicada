8a9a7777f2cee223c08b00186c868da8b4965a24
danirovi14 <142929332+danirovi14@users.noreply.github.com>
2023-10-23 19:32:20 -0500

    Update methods.py

--- 2corte/methods.py
+++ 2corte/methods.py
@@ -20,3 +20,27 @@ perimetro = circulo.perimetro()
 
 print("El área del círculo es:", area)
 print("El perímetro del círculo es:", perimetro)
+
+
+#cuadrado
+class Cuadrado:
+    def __init__(self,lado1,lado2):
+        self.lado1 = lado1
+        self.lado2 = lado2
+
+    def area(self):  
+        area = self.lado1*self.lado2
+        return area
+
+    def perimetro(self):
+        perimetro =2*(self.lado1+self.lado2)
+        return perimetro
+
+
+cuadrado = Cuadrado(4,2)
+
+area = cuadrado.area()
+perimetro = cuadrado.perimetro()
+
+print("El área del cuadrado es:", area)
+print("El perímetro del cuadrado es:", perimetro)
